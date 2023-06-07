6095b4dcdba55b6f7865cde0d3954c601dcf9c47
Evgeniy <public-judge@yandex.ru>
2023-06-07 14:52:44 +0300

    add task 4 on Home_work sem #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,9 +21,10 @@ public class Main {
         System.out.printf("Ответ n="+n);
         System.out.println();
 
-        System.out.printf("3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1 \n");
+        System.out.printf("3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE " +
+                "сохранить в массив m1 \n");
         int s=Short.MAX_VALUE;
-        System.out.printf("диапазон от "+i+" до Short.MAX_VALUE "+s);
+        System.out.printf("Диапазон от "+i+" до Short.MAX_VALUE= "+s+", кратное числу n= " +n+ ":");
         System.out.println();
         int[] calculate = new int[s];
         int count=0;
@@ -33,8 +34,7 @@ public class Main {
                 if (j!=0) count++;
             }
         }
-//        System.out.println(Arrays.toString(calculate);
-//        System.out.println(count);
+
         int[] m1=new int[count];
         int k=0;
         for (int j = 0; j < calculate.length & k< m1.length ; j++) {
@@ -44,5 +44,35 @@ public class Main {
             }
         }
         System.out.println(Arrays.toString(m1));
+        System.out.println();
+
+        System.out.printf("4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE " +
+                "до i и сохранить в массив m2 \n");
+        int min_number=Short.MIN_VALUE;
+        System.out.printf("Диапазон от "+i+" до Short.MIN_VALUE ="+min_number+", некратное числу n= " +n+ ":");
+        System.out.println();
+        int len=-1*min_number+i;
+
+
+        int[] calculate2 = new int[len];
+        int count2=0;
+        for (int j = 0; j < len && min_number<i; j++) {
+            min_number++;
+            if (min_number%n !=0) {
+                calculate2[j]=min_number;
+                if (j!=0) count2++;
+            }
+        }
+
+        int[] m2=new int[count2];
+        int l=0;
+        for (int j = 0; j < calculate2.length & l< m2.length ; j++) {
+            if (calculate2[j] != 0){
+                m2[l] = calculate2[j];
+                l++;
+            }
+        }
+        System.out.println(Arrays.toString(m2));
+
     }
 }
